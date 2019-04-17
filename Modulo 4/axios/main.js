370a02b249782c70310a6bc57224f63e88303e39
c4pote <c4pote@gmail.com>
2019-04-17 11:23:24 -0300

    Base criada com javascript puro.

---
+++ Modulo 4/axios/main.js
@@ -0,0 +1,9 @@
+axios
+  .get("https://api.github.com/users/diego3g")
+  .then(function(response) {
+    console.log(response.data.avatar_url);
+    console.log(response.data);
+  })
+  .catch(function(error) {
+    console.warn(error);
+  });
